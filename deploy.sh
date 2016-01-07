07cae56148f63733c1953a08097c751ca1763534
Mike Bryant <m@ocado.com>
2016-01-07 15:18:29 +0000

    Add promotion and testing

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,19 @@ fi
 /var/go/google-cloud-sdk/bin/gcloud --quiet components update
 /var/go/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file .gcloud-key
 
-export DATABASE_NAME="cfl_$1"
-export CACHE_PREFIX="$1-"
+export MODULE_NAME=$1
+export DATABASE_NAME="cfl_${MODULE_NAME}"
+export CACHE_PREFIX="${MODULE_NAME}-"
 export VERSION="$2"
 
 ./manage.py migrate --noinput -v 2
 
 envsubst <app.yaml.tmpl >app.yaml
 
-/var/go/google-cloud-sdk/bin/gcloud preview app --quiet --verbosity info deploy app.yaml --project $APP_ID --version $VERSION
+/var/go/google-cloud-sdk/bin/gcloud preview app --quiet --verbosity info deploy app.yaml --project ${APP_ID} --version ${VERSION}
+
+# Test the site
+./test.sh ${MODULE_NAME} ${VERSION}
+
+# Promote
+/var/go/google-cloud-sdk/bin/gcloud preview app modules set-default --project ${APP_ID} --version ${VERSION} ${MODULE_NAME}
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+
+MODULE_NAME=$1
+VERSION=$1
+
+curl -v https://${VERSION}-dot-${MODULE_NAME}-dot-${APP_ID}.appspot.com/ -o /dev/null | grep "HTTP/1.1 200"
