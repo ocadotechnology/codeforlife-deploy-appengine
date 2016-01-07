b3638470e4e881367abbd15b921f89e6f8306185
Mike Bryant <m@ocado.com>
2016-01-07 15:32:22 +0000

    Better installation of gcloud

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -ex
 
+${SNAP_CACHE_DIR}
+
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-if [ ! -d /var/go/google-cloud-sdk ]; then
-    curl https://sdk.cloud.google.com | bash
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+if [ ! -d ${SNAP_CACHE_DIR}/google-cloud-sdk ]; then
+    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz
+    tar zxf google-cloud-sdk-91.0.1-linux-x86_64.tar.gz -C ${SNAP_CACHE_DIR}
+    rm google-cloud-sdk-91.0.1-linux-x86_64.tar.gz
 fi
-/var/go/google-cloud-sdk/bin/gcloud --quiet components update
-/var/go/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file .gcloud-key
+
+GCLOUD=${SNAP_CACHE_DIR}/google-cloud-sdk/bin/gcloud
+
+${GCLOUD} --quiet components update
+${GCLOUD} auth activate-service-account --key-file .gcloud-key
 
 export MODULE_NAME=$1
 export DATABASE_NAME="cfl_${MODULE_NAME}"
@@ -16,10 +24,10 @@ export VERSION="$2"
 
 envsubst <app.yaml.tmpl >app.yaml
 
-/var/go/google-cloud-sdk/bin/gcloud preview app --quiet --verbosity info deploy app.yaml --project ${APP_ID} --version ${VERSION}
+${GCLOUD} preview app --quiet --verbosity info deploy app.yaml --project ${APP_ID} --version ${VERSION}
 
 # Test the site
 ./test.sh ${MODULE_NAME} ${VERSION}
 
 # Promote
-/var/go/google-cloud-sdk/bin/gcloud preview app modules set-default --project ${APP_ID} --version ${VERSION} ${MODULE_NAME}
+${GCLOUD} preview app modules set-default --project ${APP_ID} --version ${VERSION} ${MODULE_NAME}
