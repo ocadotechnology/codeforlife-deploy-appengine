823a7a2cf4eed6702e224cf18bbeaf9718ed87f7
Mike Bryant <m@ocado.com>
2016-01-13 16:04:41 +0000

    Fix the test script

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 MODULE_NAME=$1
 VERSION=$2
 
-curl -v https://${VERSION}-dot-${MODULE_NAME}-dot-${APP_ID}.appspot.com/ -o /dev/null | grep "HTTP/1.1 200"
+curl -v https://${VERSION}-dot-${MODULE_NAME}-dot-${APP_ID}.appspot.com/ -o /dev/null 2>&1 | grep "200 OK"
