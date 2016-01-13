aaad90b832e68b29cd5a98586ccf6b37b9a3c3c8
Mike Bryant <m@ocado.com>
2016-01-13 12:16:25 +0000

    More debugging

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 import logging
+import os
 import sys
 
 logging.error("sys.path: %s", sys.path)
+logging.error("ls: %s", os.listdir('.')
+logging.error("ls: %s", os.listdir('webapp')
 
 try:
     import webapp.wsgi
