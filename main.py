d87cdfc3467d7a3edacd20595c9ac8f44838e067
Mike Bryant <m@ocado.com>
2016-01-13 12:07:05 +0000

    Add some debugging for appengine

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import webapp.wsgi
+
+import logging
+import sys
+
+logging.error("sys.path: %s", sys.path)
+
+try:
+    import webapp.wsgi
+except ImportError as err:
+    logging.error("ImportError: %s", err)
+    raise
 
 application = webapp.wsgi.application
