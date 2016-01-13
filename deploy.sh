9681590ecbd0b530446b5dbf9afa860e691866aa
Mike Bryant <m@ocado.com>
2016-01-13 20:10:00 +0000

    Add gcloud debug again

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ export CACHE_PREFIX="${MODULE_NAME}-${VERSION}-"
 
 envsubst <app.yaml.tmpl >app.yaml
 
-${GCLOUD} preview app --quiet deploy app.yaml --project ${APP_ID} --version ${VERSION} --no-promote
+${GCLOUD} preview app --quiet deploy app.yaml --project ${APP_ID} --version ${VERSION} --no-promote --verbosity debug
 
 # Test the site
 ./test.sh ${MODULE_NAME} ${VERSION}
