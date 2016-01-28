fe80995c8bf5b46c3f126215502fdf387bd59e5e
Mike Bryant <m@ocado.com>
2016-01-28 18:37:21 +0000

    Revert "Switch back to appcfg.py for deployment" - testing fix for 462
    This reverts commit b7d19b33709822c7df2637d089c1302041d6f7a8.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -ex
 
-### OLD - remove when google-462 is fixed
-curl -O -s https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.27.zip
-sudo unzip -qq google_appengine_1.9.27.zip -d /opt/
-rm google_appengine_1.9.27.zip
-export PATH=/opt/google_appengine:$PATH
-###
-
-
-
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
@@ -33,11 +24,7 @@ export CACHE_PREFIX="${MODULE_NAME}-${VERSION}-"
 
 envsubst <app.yaml.tmpl >app.yaml
 
-### OLD - remove when 462 is fixed
-appcfg.py update --oauth2_refresh_token=${GAE_OAUTH_TOKEN} -V ${VERSION} -A ${APP_ID} app.yaml
-### New:
-#${GCLOUD} preview app --quiet deploy app.yaml --project ${APP_ID} --version ${VERSION} --no-promote --verbosity debug
-###
+${GCLOUD} preview app --quiet deploy app.yaml --project ${APP_ID} --version ${VERSION} --no-promote --verbosity debug
 
 # Test the site
 ./test.sh ${MODULE_NAME} ${VERSION}
