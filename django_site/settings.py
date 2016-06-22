b9e1d65b0513f67a6b1ac67032f15e3128f9424f
Mike Bryant <mike@mikebryant.me.uk>
2016-06-22 21:28:15 +0100

    Update staging location

--- django_site/settings.py
+++ django_site/settings.py
@@ -244,7 +244,7 @@ MIGRATION_MODULES = {
     'djangocms_video': 'djangocms_video.migrations_django',
 }
 
-AIMMO_GAME_SERVER_LOCATION_FUNCTION = lambda game: ('http://130.211.21.203', "/game/%s/socket.io" % game)
+AIMMO_GAME_SERVER_LOCATION_FUNCTION = lambda game: ('http://staging.aimmo.codeforlife.education', "/game/%s/socket.io" % game)
 
 # Keep this at the bottom
 from django_autoconfig.autoconfig import configure_settings
