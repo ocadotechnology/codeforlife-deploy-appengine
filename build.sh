50882a38e6712683ed3c09b9b3f1c92df16ba442
Celine Boudier <boudier.celine@gmail.com>
2017-04-11 11:54:01 +0100

    upgrade cms thingies

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ pip install -t lib codeforlife-portal aimmo
 
 rm -rf lib/pytz lib/pytz*.dist-info
 
-pip install 'html5<0.99'
+#pip install 'html5<0.99'
+pip install django-cms --upgrade
+pip install djangocms-text-ckeditor --upgrade
+
 ./manage.py collectstatic --noinput
 
