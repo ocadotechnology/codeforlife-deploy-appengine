2008e26bc38970e80fdeb9582befcf761c319ec1
Celine Boudier <boudier.celine@gmail.com>
2017-04-11 11:56:09 +0100

    cleaning lib folder

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ rbenv rehash
 pip install -t lib codeforlife-portal aimmo
 
 rm -rf lib/pytz lib/pytz*.dist-info
+rm -rf lib/djangocms_text_ckeditor lib/djangocms_text_ckeditor*.dist-info
+
 
 #pip install 'html5<0.99'
 pip install django-cms --upgrade
