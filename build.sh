9740a3201eabe1ac8c28ca6d58bfd670f9f17140
Celine Boudier <boudier.celine@gmail.com>
2017-04-11 14:46:05 +0100

    trying to make it find the package somewhere else

--- build.sh
+++ build.sh
@@ -6,12 +6,15 @@ rbenv rehash
 pip install -t lib codeforlife-portal aimmo
 
 rm -rf lib/pytz lib/pytz*.dist-info
-rm -rf lib/djangocms_text_ckeditor lib/djangocms_text_ckeditor*.dist-info
-
+#rm -rf lib/djangocms_text_ckeditor lib/djangocms_text_ckeditor*.dist-info
+rm -rf /home/rof/.virtualenv/local/lib/python2.7/site-packages/djangocms_text_ckeditor
+rm -rf /home/rof/.virtualenv/local/lib/python2.7/site-packages/djangocms_text_ckeditor*.dist-info
 
 #pip install 'html5<0.99'
 #pip install django-cms --upgrade
 #pip install djangocms-text-ckeditor --upgrade
+
+#echo $PATH
 
 ./manage.py collectstatic --noinput
 
