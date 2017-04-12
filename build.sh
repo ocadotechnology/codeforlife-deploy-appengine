ce9b888f4a796c433f178e123efd68e0b3258492
Celine Boudier <boudier.celine@gmail.com>
2017-04-12 14:23:56 +0100

    logs, logs everywhere

--- build.sh
+++ build.sh
@@ -7,14 +7,17 @@ pip install -t lib codeforlife-portal aimmo
 
 rm -rf lib/pytz lib/pytz*.dist-info
 #rm -rf lib/djangocms_text_ckeditor lib/djangocms_text_ckeditor*.dist-info
-rm -rf /home/rof/.virtualenv/local/lib/python2.7/site-packages/djangocms_text_ckeditor
-rm -rf /home/rof/.virtualenv/local/lib/python2.7/site-packages/djangocms_text_ckeditor*.dist-info
+#rm -rf /home/rof/.virtualenv/local/lib/python2.7/site-packages/djangocms_text_ckeditor
+#rm -rf /home/rof/.virtualenv/local/lib/python2.7/site-packages/djangocms_text_ckeditor*.dist-info
 
 #pip install 'html5<0.99'
 #pip install django-cms --upgrade
 #pip install djangocms-text-ckeditor --upgrade
 
 #echo $PATH
+
+ls -a lib/
+ls -a /home/rof/.virtualenv/local/lib/python2.7/site-packages/
 
 ./manage.py collectstatic --noinput
 
