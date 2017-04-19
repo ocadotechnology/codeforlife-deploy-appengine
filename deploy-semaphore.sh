78f2df3d321435f7cd0375b4c261b4e422e7af23
Celine Boudier <boudier.celine@gmail.com>
2017-04-19 16:20:35 +0100

    using cloud sql proxy to use database (waiting for semaphoreci IP to whitelist it)

--- deploy-semaphore.sh
+++ deploy-semaphore.sh
@@ -4,6 +4,7 @@ export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
 GCLOUD=$SEMAPHORE_CACHE_DIR/google-cloud-sdk/bin/gcloud
+SQL_PROXY=cloud_sql_proxy
 
 if [ ! -x ${GCLOUD} ]; then
     wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz
@@ -12,13 +13,21 @@ if [ ! -x ${GCLOUD} ]; then
 fi
 
 ${GCLOUD} --quiet components update
-${GCLOUD} auth activate-service-account --key-file .gcloud-key
+#${GCLOUD} auth activate-service-account --key-file .gcloud-key
+
+if [ ! -x ${SQL_PROXY} ]; then
+    wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
+    mv cloud_sql_proxy.linux.amd64 cloud_sql_proxy
+    chmod +x cloud_sql_proxy
+fi
 
 export MODULE_NAME=$1
 export VERSION="$2"
 export DATABASE_POSTFIX="$3"
 export DATABASE_NAME="cfl_${DATABASE_POSTFIX}"
 export CACHE_PREFIX="${MODULE_NAME}-${VERSION}-"
+
+./cloud_sql_proxy -instances=$CLOUD_SQL_NAME=tcp:3306 -credential_file=.gcloud-key
 
 ./manage.py migrate --noinput
 
