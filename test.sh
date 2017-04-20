e78d7fa29613fe017899466888c2782d00789f8b
Celine Boudier <boudier.celine@gmail.com>
2017-04-20 15:41:53 +0100

    test

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 MODULE_NAME=$1
 VERSION=$2
 
-curl -v https://${VERSION}-dot-${MODULE_NAME}-dot-${APP_ID}.appspot.com/ -o /dev/null 2>&1 | grep "200 OK"
+curl -v https://${VERSION}-dot-${MODULE_NAME}-dot-${APP_ID}.appspot.com/ -o /dev/null 2>&1
+#| grep "200 OK"
