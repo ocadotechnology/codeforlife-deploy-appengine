3eda765656c04104ab7b62a4c1d9c0ab9c4a0af6
Niket Shah <masterniket@gmail.com>
2017-12-18 16:03:58 +0000

    Fetch AIMMO Unity release and copy into right directory

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 gem install sass --version 3.3.4
 rbenv rehash
+pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 
 pip install -t lib codeforlife-portal aimmo
+
+python get_latest_aimmo_unity_release.py
 
 rm -rf lib/pytz lib/pytz*.dist-info
 
