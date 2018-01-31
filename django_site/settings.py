0c0df34e1b55cc636d859db42cb743771eedbaaf
Niket Shah <masterniket@gmail.com>
2018-01-31 19:14:10 +0000

    add django_forms_bootstrap

--- django_site/settings.py
+++ django_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_countries',
+    'django_forms_bootstrap'
     'rest_framework',
     'online_status',
 
