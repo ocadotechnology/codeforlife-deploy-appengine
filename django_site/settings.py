20640e2363ea54055a1b149ef87c7642c1d17d9b
Olaf Szmidt <szmidtolaf@gmail.com>
2018-01-31 19:17:01 +0000

    Update settings.py

--- django_site/settings.py
+++ django_site/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_countries',
-    'django_forms_bootstrap'
+    'django_forms_bootstrap',
     'rest_framework',
     'online_status',
 
