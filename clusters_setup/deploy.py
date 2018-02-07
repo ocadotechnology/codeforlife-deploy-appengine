b91e7e41ce955245118550e58644c3982b4e49b0
Olaf Szmidt <szmidtolaf@gmail.com>
2018-02-07 10:35:56 +0000

    Specify config file into load kube config

--- clusters_setup/deploy.py
+++ clusters_setup/deploy.py
@@ -67,7 +67,7 @@ def main(module_name):
     """
     :param module_name: The environment (ie. staging, etc).
     """
-    kubernetes.config.load_kube_config(os.path.join(BASE_DIR, ".gcloud-key"))
+    kubernetes.config.load_kube_config("/home/runner/.kube/config")
 
     global api_instance
     global extensions_api_instance
