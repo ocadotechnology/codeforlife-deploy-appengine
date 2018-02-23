15a69a6279e3c579cd6b472ad011cc0c4f83534b
Celine Boudier <boudier.celine@gmail.com>
2018-02-23 11:24:46 +0000

    migrations back - IPs added to whitelist from semaphore ci gist

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ ${GCLOUD} container clusters get-credentials ${MODULE_NAME} --zone europe-west1-
 python clusters_setup/deploy.py "${MODULE_NAME}"
 
 
-#./manage.py migrate --noinput
+./manage.py migrate --noinput
 
 envsubst <app.yaml.tmpl >app.yaml
 
