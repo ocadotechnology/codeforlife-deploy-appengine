0c91ac2875a7afc5e10190341c95777b2b3e17e7
Niket Shah <masterniket@gmail.com>
2018-02-28 10:49:26 +0000

    Different recaptcha keys based on module
    Also using recaptcha branch for testing

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ gem install sass --version 3.3.4
 rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 
-pip install -t lib codeforlife-portal aimmo
+pip install -t lib git+https://github.com/mrniket/codeforlife-portal@recaptcha_update
+
+pip install -t lib aimmo
 
 python get_latest_aimmo_unity_release.py
 
 rm -rf lib/pytz lib/pytz*.dist-info
 
 ./manage.py collectstatic --noinput
-
