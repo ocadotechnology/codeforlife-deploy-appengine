916ca102147772ba0bc7758d140fad5227560e57
Celine Boudier <boudier.celine@gmail.com>
2018-04-24 15:10:01 +0100

    Revert "Added reference to context processor in settings.py (#92)"
    This reverts commit e0e3e6cdeca28198372cd1df846276f1c5400703.

--- django_site/settings.py
+++ django_site/settings.py
@@ -208,7 +208,6 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
                 'sekizai.context_processors.sekizai',
-                'portal.context_processors.process_newsletter_form',
             ],
         },
     },
