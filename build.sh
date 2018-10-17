b3d228a50dbb46106014cf1c8a24e019b6bb0c46
Niket Shah <masterniket@gmail.com>
2018-10-17 09:18:26 +0100

    Fix urllib3 to 1.22 (#134)
    * install a working urllib3

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ export ENVIRONMENT="$1"
 gem install sass --version 3.3.4
 rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
+pip install "urllib3==1.22" --force-reinstall
 
 pip install -t lib codeforlife-portal
 
