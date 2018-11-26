a3b2d01d781c1ac96051d47347488969c2b9b69c
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2018-11-26 11:32:45 +0000

    get from git branch

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 pip install "urllib3==1.22" --force-reinstall
 
 pip install -t lib codeforlife-portal
-
-if [ "$ENVIRONMENT" = "default" ]
-then
-    pip install -t lib aimmo
-else
-    pip install -t lib aimmo --pre
-fi
+pip install -t lib git+https://github.com/ocadotechnology/aimmo.git@fix_kubernetes_mode
+# if [ "$ENVIRONMENT" = "default" ]
+# then
+#     pip install -t lib aimmo
+# else
+#     pip install -t lib aimmo --pre
+# fi
 
 python get_latest_aimmo_unity_release.py
 
