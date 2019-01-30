f9285a52649339b650143c944d392712b3167bdb
Paris Goldman-Smith <parisgoldmansmith@googlemail.com>
2019-01-30 11:33:22 +0000

    get from git branch

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 pip install "urllib3==1.22" --force-reinstall
 
 pip install -t lib codeforlife-portal
-if [ "$ENVIRONMENT" = "default" ]
-then
-    pip install -t lib --upgrade --no-deps aimmo
-else
-    pip install -t lib --pre --upgrade --no-deps aimmo
-fi
+pip install -t lib git+https://github.com/ocadotechnology/aimmo.git@custom_metrics
+# if [ "$ENVIRONMENT" = "default" ]
+# then
+#     pip install -t lib --upgrade --no-deps aimmo
+# else
+#     pip install -t lib --pre --upgrade --no-deps aimmo
+# fi
 
 python get_latest_aimmo_unity_release.py
 
