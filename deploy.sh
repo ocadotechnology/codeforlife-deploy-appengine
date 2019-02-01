b4bf0cb5b49a9f1fd4fd9303bc51a5b294f67c2a
faucomte97 <f.aucomte@hotmail.co.uk>
2019-02-01 18:10:19 +0000

    Revert migrate code to default code.

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ ${GCLOUD} container clusters get-credentials ${MODULE_NAME} --zone europe-west1-
 python clusters_setup/deploy.py "${MODULE_NAME}"
 
 
-./manage.py migrate game 0067_level_score_27 --no-input
+./manage.py migrate --no-input
 
 envsubst <app.yaml.tmpl >app.yaml
 
