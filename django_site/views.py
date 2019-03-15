dd9209cd1d66565706b58c23c091ac280bd0d346
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-03-15 13:53:27 +0000

    Point to semver implementation instead of versioneer

--- django_site/views.py
+++ django_site/views.py
@@ -1,13 +1,14 @@
 from django.http import JsonResponse
 
-import game._version
 import aimmo._version
-import portal._version
+import game
+import portal
+
 
 def versions(_request):
-    '''Return json containing the installed versions of the main packages.'''
+    """Return json containing the installed versions of the main packages."""
     return JsonResponse({
         'aimmo': aimmo._version.get_versions()['full-revisionid'],
-        'codeforlife-portal': portal._version.get_versions()['full-revisionid'],
-        'rapid-router': game._version.get_versions()['full-revisionid'],
+        'codeforlife-portal': portal.__version__,
+        'rapid-router': game.__version__
     })
