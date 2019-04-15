87709e1d90fdbbd011abdc17442ae99302ac858e
Niket Shah <masterniket@gmail.com>
2019-04-15 16:22:41 +0100

    update urls file to Django 1.10 style
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- django_site/urls.py
+++ django_site/urls.py
@@ -2,24 +2,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django_site.views import versions
+from portal import urls as portal_urls
+from game import urls as game_urls
+from aimmo import urls as aimmo_urls
+from reports import urls as repors_urls
 
-js_info_dict = {
-    'packages': ('conf.locale',),
-}
+js_info_dict = {"packages": ("conf.locale",)}
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^', include('portal.urls')),
-    url(r'^administration/', include(admin.site.urls)),
-    url(r'^rapidrouter/', include('game.urls')),
-    url(r'^reports/', include('reports.urls')),
-    url(r'^aimmo/', include('aimmo.urls')),
-    url(r'^versions/$', versions, name='versions'),
-)
-
-try:
-    import django_pandasso
-    urlpatterns = urlpatterns + patterns(url(r'^django-pandasso/', include('django_pandasso.urls')))
-except ImportError:
-    pass
+urlpatterns = [
+    url(r"^", include(portal_urls)),
+    url(r"^administration/", include(admin.site.urls)),
+    url(r"^rapidrouter/", include(game_urls)),
+    url(r"^reports/", include(reports_urls)),
+    url(r"^aimmo/", include(aimmo_urls)),
+    url(r"^versions/$", versions, name="versions"),
+]
