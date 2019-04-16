b42a485e65443105b6f357913182a8acad3e22f7
Niket Shah <masterniket@gmail.com>
2019-04-16 09:31:31 +0100

    fix typo
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- django_site/urls.py
+++ django_site/urls.py
@@ -5,7 +5,7 @@ from django_site.views import versions
 from portal import urls as portal_urls
 from game import urls as game_urls
 from aimmo import urls as aimmo_urls
-from reports import urls as repors_urls
+from reports import urls as reports_urls
 
 js_info_dict = {"packages": ("conf.locale",)}
 
