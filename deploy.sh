64108ec5e9c535a681a025ecef4df3be517351cd
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-07-02 13:04:54 +0100

    uncommented migration

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ ${GCLOUD} container clusters get-credentials ${MODULE_NAME} --zone europe-west1-
 python clusters_setup/deploy.py "${MODULE_NAME}"
 
 
-# ./manage.py migrate --no-input
+./manage.py migrate --no-input
 
 envsubst <app.yaml.tmpl >app.yaml
 
