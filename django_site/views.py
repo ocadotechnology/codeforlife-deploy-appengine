dfd07e9d19b8f741b0f79fc5f55b836f16f0018a
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-08-13 08:16:14 +0100

    Point to AIMMO's new version implementation

--- django_site/views.py
+++ django_site/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-import aimmo._version
+import aimmo
 import game
 import portal
 
@@ -8,7 +8,7 @@ import portal
 def versions(_request):
     """Return json containing the installed versions of the main packages."""
     return JsonResponse({
-        'aimmo': aimmo._version.get_versions()['full-revisionid'],
+        'aimmo': aimmo.__version__,
         'codeforlife-portal': portal.__version__,
         'rapid-router': game.__version__
     })
