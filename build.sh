83e1e0f40ed47d0c9d8569868fc9f4df08016b78
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2019-11-19 10:00:22 +0000

    Install gaerpytz to fix timezone issue (#194)
    * Install gaerpytz to fix timezone issue
    
    * Get gaerpytz zipfile to overwrite pytz
    
    * Update script to use requests only
    
    * Install libraries needed for script

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ export ENVIRONMENT="$1"
 
 gem install sass --version 3.3.4
 rbenv rehash
-pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
-pip install "urllib3==1.22" --force-reinstall
+pip install beautifulsoup4
+pip install requests
 
 pip install -t lib codeforlife-portal
 if [ "$ENVIRONMENT" = "default" ]
@@ -14,5 +14,7 @@ then
 else
     pip install -t lib --pre --upgrade --no-deps aimmo
 fi
+
+python install_gaerpytz.py
 
 ./manage.py collectstatic --noinput
---
+++ install_gaerpytz.py
@@ -0,0 +1,21 @@
+import StringIO
+import zipfile
+
+import requests
+from bs4 import BeautifulSoup
+
+# Get HTML of the gaerpytz home page
+gaerpytz_url = "https://gaerpytz.appspot.com"
+gaerpytz_webpage = requests.get(gaerpytz_url)
+gaerpytz_html = gaerpytz_webpage.text
+
+# Parse HTML to get the zipfile's download link
+soup = BeautifulSoup(gaerpytz_html, "html.parser")
+zip_href = soup.find(title="Download the latest build").get("href")
+zip_url = gaerpytz_url + zip_href
+
+# Download the zipfile and extract it to the lib folder. The zipfile contains
+# the module folder for pytz and will overwrite the one currently in lib
+request = requests.get(zip_url, stream=True)
+zip_file = zipfile.ZipFile(StringIO.StringIO(request.content))
+zip_file.extractall("lib")
