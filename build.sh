e28359ed25e75b8157680e1c17673871dd896176
Florian Aucomte <florian.aucomt1@ocado.com>
2019-12-17 01:31:26 +0000

    Get aimmo branch

--- build.sh
+++ build.sh
@@ -9,12 +9,13 @@ pip install requests
 pip install -t lib requests-toolbelt
 
 pip install -t lib codeforlife-portal
-if [ "$ENVIRONMENT" = "default" ]
-then
-    pip install -t lib --upgrade --no-deps aimmo
-else
-    pip install -t lib --pre --upgrade --no-deps aimmo
-fi
+pip install -t lib git+https://github.com/ocadotechnology/aimmo.git@change_bundling_order
+#if [ "$ENVIRONMENT" = "default" ]
+#then
+#    pip install -t lib --upgrade --no-deps aimmo
+#else
+#    pip install -t lib --pre --upgrade --no-deps aimmo
+#fi
 
 python install_gaerpytz.py
 
