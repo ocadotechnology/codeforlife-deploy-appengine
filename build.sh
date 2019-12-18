ebe6ddc042ae80ad10fd260f12190452217b25a2
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2019-12-18 11:19:19 +0000

    fix: Remove _sass.so file (#205)
    * Get aimmo branch
    
    * Remove sass install
    
    * Don't install aimmo
    
    * Don't install portal
    
    * Uninstall libsass
    
    * Remove generated sass so file
    
    * Delete sass file after static collection

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 export ENVIRONMENT="$1"
 
-gem install sass --version 3.3.4
 rbenv rehash
 pip install beautifulsoup4
 pip install requests
@@ -19,3 +18,5 @@ fi
 python install_gaerpytz.py
 
 ./manage.py collectstatic --noinput
+
+rm ./lib/_sass.so
