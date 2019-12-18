117a214f81070f8913d4115ef212d9fc72a61058
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-12-18 10:38:09 +0000

    Don't install portal

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ pip install beautifulsoup4
 pip install requests
 pip install -t lib requests-toolbelt
 
-pip install -t lib codeforlife-portal
-#if [ "$ENVIRONMENT" = "default" ]
-#then
-#    pip install -t lib --upgrade --no-deps aimmo
-#else
-#    pip install -t lib --pre --upgrade --no-deps aimmo
-#fi
+#pip install -t lib codeforlife-portal
+if [ "$ENVIRONMENT" = "default" ]
+then
+    pip install -t lib --upgrade --no-deps aimmo
+else
+    pip install -t lib --pre --upgrade --no-deps aimmo
+fi
 
 python install_gaerpytz.py
 
