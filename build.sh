2bddac99102c1b930d2ce76fad9924f22dd249de
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-12-18 10:41:07 +0000

    Uninstall libsass

--- build.sh
+++ build.sh
@@ -7,13 +7,15 @@ pip install beautifulsoup4
 pip install requests
 pip install -t lib requests-toolbelt
 
-#pip install -t lib codeforlife-portal
+pip install -t lib codeforlife-portal
 if [ "$ENVIRONMENT" = "default" ]
 then
     pip install -t lib --upgrade --no-deps aimmo
 else
     pip install -t lib --pre --upgrade --no-deps aimmo
 fi
+
+pip uninstall libsass
 
 python install_gaerpytz.py
 
