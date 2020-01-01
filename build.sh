1cdacb30039d00f501bc0c7608e0291c8424d56e
Niket <niket.shah1@ocado.com>
2020-01-01 22:26:11 +0000

    no build sh
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 export ENVIRONMENT="$1"
 
-rbenv rehash
-pip install beautifulsoup4
-pip install requests
-pip install -t lib requests-toolbelt
+# rbenv rehash
+# pip install beautifulsoup4
+# pip install requests
+# pip install -t lib requests-toolbelt
 
-pip install -t lib codeforlife-portal
-if [ "$ENVIRONMENT" = "default" ]
-then
-    pip install -t lib --upgrade --no-deps aimmo
-else
-    pip install -t lib --pre --upgrade --no-deps aimmo
-fi
+# pip install -t lib codeforlife-portal
+# if [ "$ENVIRONMENT" = "default" ]
+# then
+#     pip install -t lib --upgrade --no-deps aimmo
+# else
+#     pip install -t lib --pre --upgrade --no-deps aimmo
+# fi
 
-python install_gaerpytz.py
+# python install_gaerpytz.py
 
-./manage.py collectstatic --noinput
+# ./manage.py collectstatic --noinput
