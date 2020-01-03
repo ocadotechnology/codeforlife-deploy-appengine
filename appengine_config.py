b7a52bb692d8848196cb0ea2a48944b2424eb5c7
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-01-03 12:33:29 +0000

    Revert back to lib installs

--- appengine_config.py
+++ appengine_config.py
@@ -4,11 +4,11 @@ starts a new instance of your application. This runs before any
 WSGI applications specified in app.yaml are loaded.
 """
 
-# from google.appengine.ext import vendor
-#
-# # Third-party libraries are stored in "lib", vendoring will make
-# # sure that they are importable by the application.
-# vendor.add('lib')
+from google.appengine.ext import vendor
+
+# Third-party libraries are stored in "lib", vendoring will make
+# sure that they are importable by the application.
+vendor.add('lib')
 
 from requests_toolbelt.adapters import appengine
 
