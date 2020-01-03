35437e37f156452a7276fe93277cfb79c979a10b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-01-03 13:19:48 +0000

    Try from lib import

--- django_site/wsgi.py
+++ django_site/wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
 
-from django.core.wsgi import get_wsgi_application
+from lib.django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
