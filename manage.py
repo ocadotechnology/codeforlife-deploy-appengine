5d4c10ae555ad984a6ff7f5d3b450e53e8f55c73
Niket Shah <masterniket@gmail.com>
2020-03-12 16:03:44 +0000

    put import in main
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- manage.py
+++ manage.py
@@ -5,19 +5,19 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
-try:
-    import googleclouddebugger
-    LOGGER.info("got here")
-    print("got here by print")
-    googleclouddebugger.enable()
-except ImportError:
-    LOGGER.info("dammit")
-    print("dammit by print")
-    pass
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
 if __name__ == "__main__":
+    try:
+        import googleclouddebugger
+        LOGGER.info("got here")
+        print("got here by print")
+        googleclouddebugger.enable()
+    except ImportError:
+        LOGGER.info("dammit")
+        print("dammit by print")
+        pass
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
 
     from django.core.management import execute_from_command_line
