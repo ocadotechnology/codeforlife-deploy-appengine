334bace9dda3d6e7a3616629af69696ac33052c7
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-03-12 14:23:00 +0000

    Setup Google Cloud debugger

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+try:
+    import googleclouddebugger
+
+    googleclouddebugger.enable()
+except ImportError:
+    pass
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
