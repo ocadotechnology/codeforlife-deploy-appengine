154f89067c9b2eade60ac71455ceb4a4265417a8
Niket Shah <niket.shah1@ocado.com>
2020-03-13 10:11:27 +0000

    Setup Python cloud debugger (#219)
    * Setup Google Cloud debugger
    
    * Add new line
    
    * Remove comma
    
    * Remove comma part 2
    
    * add logging
    
    Signed-off-by: Niket Shah <masterniket@gmail.com>
    
    * turn on secure ssl
    
    Signed-off-by: Niket Shah <masterniket@gmail.com>
    
    * add logs for import error case
    
    Signed-off-by: Niket Shah <masterniket@gmail.com>
    
    * put import in main
    
    Signed-off-by: Niket Shah <masterniket@gmail.com>
    
    * try putting debugging setup in appengine_configpy
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * put in main.py
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * define LOGGER
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * import logging
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * clean up code
    
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from django_site.wsgi import application
 
+try:
+    import googleclouddebugger
+
+    googleclouddebugger.enable()
+except ImportError:
+    pass
+
 app = application
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
