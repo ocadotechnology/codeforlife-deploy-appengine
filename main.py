14d34e2651f0273caa31608eef7080e0eb658824
Niket <niket.shah1@ocado.com>
2020-03-13 09:58:12 +0000

    clean up code
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from django_site.wsgi import application
-import logging
-
-LOGGER = logging.getLogger(__name__)
 
 try:
     import googleclouddebugger
 
-    LOGGER.info("got here")
-    print("got here by print")
     googleclouddebugger.enable()
 except ImportError:
-    LOGGER.info("dammit")
-    print("dammit by print")
     pass
 
 app = application
--- manage.py
+++ manage.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import logging
-
-LOGGER = logging.getLogger(__name__)
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
 if __name__ == "__main__":
-    try:
-        import googleclouddebugger
-        LOGGER.info("got here")
-        print("got here by print")
-        googleclouddebugger.enable()
-    except ImportError:
-        LOGGER.info("dammit")
-        print("dammit by print")
-        pass
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
 
     from django.core.management import execute_from_command_line
