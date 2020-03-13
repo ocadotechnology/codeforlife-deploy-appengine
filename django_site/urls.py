5ca15d0e24d907ffb83408eafc2d3897104e0b5f
Niket <niket.shah1@ocado.com>
2020-03-13 14:16:44 +0000

    remove lib. from django_site
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- django_site/urls.py
+++ django_site/urls.py
@@ -1,6 +1,6 @@
 from lib.django.conf.urls import include, url
 from lib.django.contrib import admin
-from lib.django_site.views import versions
+from django_site.views import versions
 
 from lib.portal import urls as portal_urls
 from lib.game import urls as game_urls
