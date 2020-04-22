87bbb266326e7be0ac8ecb1c58a259b47bad182f
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-04-22 13:04:03 +0100

    Add branches to requirements file

--- generate_requirements.py
+++ generate_requirements.py
@@ -2,7 +2,8 @@ from lib.portal import __version__ as portal_version
 from lib.aimmo import __version__ as aimmo_version
 
 requirements = (
-    f"codeforlife-portal=={portal_version}\n"
+    f"git+https://github.com/ocadotechnology/rapid-router@cypress_smoke_tests#egg=rapid-router\n"
+    f"git+https://github.com/ocadotechnology/codeforlife-portal@cypress_smoke_tests#egg=codeforlife-portal\n"
     f"aimmo=={aimmo_version}\n"
     f"requests-toolbelt==0.9.*\n"
     f"mysqlclient==1.4.*\n"
