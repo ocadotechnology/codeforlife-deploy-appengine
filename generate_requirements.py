8f720db1fecca995e3abdfdb39f427c1d36d5ebe
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-07-22 17:02:40 +0100

    Fix requirements

--- generate_requirements.py
+++ generate_requirements.py
@@ -2,14 +2,16 @@ from lib.portal import __version__ as portal_version
 from lib.aimmo import __version__ as aimmo_version
 
 requirements = (
-    f"codeforlife-portal=={portal_version}\n"
-    f"aimmo=={aimmo_version}\n"
+    f"git+https://github.com/ocadotechnology/rapid-router@common-test#egg=rapid-router\n"
+    f"git+https://github.com/ocadotechnology/codeforlife-portal@common-models-test#egg=codeforlife-portal\n"
+    f"git+https://github.com/ocadotechnology/aimmo@add_class_to_game#egg=aimmo\n"
     f"requests-toolbelt==0.9.*\n"
     f"mysqlclient==1.4.*\n"
     f"redis==3.3.*\n"
     f"django-redis==4.11.*\n"
     f"django-anymail[amazon_ses]==7.0.*\n"
-    f"google-python-cloud-debugger"
+    f"google-python-cloud-debugger",
+    f"cfl-common-test"
 )
 
 requirements_path = "requirements.txt"
