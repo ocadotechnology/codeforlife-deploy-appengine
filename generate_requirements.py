8d66e6b228966b73f00da9a58aa14f45abcbb1f5
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-07-23 11:11:44 +0100

    Add test pypi package to requirements

--- generate_requirements.py
+++ generate_requirements.py
@@ -10,7 +10,8 @@ requirements = (
     f"redis==3.3.*\n"
     f"django-redis==4.11.*\n"
     f"django-anymail[amazon_ses]==7.0.*\n"
-    f"google-python-cloud-debugger"
+    f"google-python-cloud-debugger\n"
+    f"https://test-files.pythonhosted.org/packages/00/a8/08be861e496afbb359626fcb2aa52838cac0ff1aeb0ccb4be3d17441776d/cfl_common_test-0.0.0-py3-none-any.whl"
 )
 
 requirements_path = "requirements.txt"
