6e686f96d027450be3bea262a8ca5639e82fc3cd
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-04 12:43:27 +0100

    Also fake 0015

--- deploy.sh
+++ deploy.sh
@@ -34,6 +34,7 @@ ${GCLOUD} container clusters get-credentials ${MODULE_NAME} --zone europe-west1-
 python clusters_setup/deploy.py "${MODULE_NAME}"
 
 ./manage.py migrate --fake aimmo 0014
+./manage.py migrate --fake aimmo 0015
 
 ./manage.py migrate --no-input
 
