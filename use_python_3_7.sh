bf184b5524e1fe24de874a173b0e0aefb15e0254
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-19 15:48:32 +0100

    Remove echos + add new line

--- use_python_3_7.sh
+++ use_python_3_7.sh
@@ -2,12 +2,12 @@
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-
-exec "$SHELL"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
 
 pyenv install 3.7.7
 
