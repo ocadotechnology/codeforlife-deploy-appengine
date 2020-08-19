820d88fe5b84f6c08bb7674c888c8ea3352edfac
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-19 17:33:13 +0100

    Install Python dependencies

--- use_python_3_7.sh
+++ use_python_3_7.sh
@@ -15,6 +15,9 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
+apt-get update
+apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 pyenv install 3.7.7
 
 pyenv global 3.7.7
