6fcf6d6e7e758f30bd3e15c6918dcff57f4d93ce
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-19 17:43:07 +0100

    Install with -y

--- use_python_3_7.sh
+++ use_python_3_7.sh
@@ -16,7 +16,7 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 
 sudo apt-get update
-sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get -y install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 pyenv install 3.7.7
 
