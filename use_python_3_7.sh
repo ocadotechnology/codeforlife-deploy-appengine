955fea9dd91a4051700757dd9b443959a92272ac
Niket <niket.shah1@ocado.com>
2020-08-20 09:29:26 +0100

    install openssl
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- use_python_3_7.sh
+++ use_python_3_7.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -ex
 
+rm  -rf ~/.pyenv
+
+curl https://pyenv.run | bash
+
+# Install OpenSSL
+wget https://www.openssl.org/source/openssl-1.1.1f.tar.gz
+tar xzvf openssl-1.1.1f.tar.gz
+cd openssl-1.1.0f
+./Configure
+# ./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
+make
+sudo make install
+
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+eval "$(pyenv init -)"
 
 pyenv install 3.7.7
 
