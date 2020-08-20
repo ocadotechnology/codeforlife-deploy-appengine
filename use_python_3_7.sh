8d71a052ccd08cfe455b06d14f8508ff45a3c291
Niket <niket.shah1@ocado.com>
2020-08-20 08:49:02 +0100

    use preexisting pyenv install
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- use_python_3_7.sh
+++ use_python_3_7.sh
@@ -1,22 +1,11 @@
 #!/bin/bash -ex
 
-cd ~
-
-rm -r .pyenv
-
-ls -a
-
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
-
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev openssl
 
 pyenv install 3.7.7
 
