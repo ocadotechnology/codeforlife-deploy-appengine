b062588cc2b7296e14b702bba76d82a31114338f
Niket <niket.shah1@ocado.com>
2020-10-30 11:20:55 +0000

    fix typo
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- generate_requirements.py
+++ generate_requirements.py
@@ -1,19 +1,21 @@
 from lib.portal import __version__ as portal_version
 from lib.aimmo import __version__ as aimmo_version
 
-requirements = [
-    # f"codeforlife-portal=={portal_version}\n"
-    "git+https://github.com/ocadotechnology/codeforlife-portal.git@debug_branch2",
-    f"aimmo=={aimmo_version}",
-    "requests-toolbelt==0.9.*",
-    "mysqlclient==1.4.*",
-    "redis==3.3.*",
-    "django-redis==4.11.*",
-    "django-anymail[amazon_ses]==7.0.*",
-    "google-python-cloud-debugger",
-    "google-cloud-logging",
-    "google-auth",
-].join("\n")
+requirements = "\n".join(
+    [
+        # f"codeforlife-portal=={portal_version}\n"
+        "git+https://github.com/ocadotechnology/codeforlife-portal.git@debug_branch2",
+        f"aimmo=={aimmo_version}",
+        "requests-toolbelt==0.9.*",
+        "mysqlclient==1.4.*",
+        "redis==3.3.*",
+        "django-redis==4.11.*",
+        "django-anymail[amazon_ses]==7.0.*",
+        "google-python-cloud-debugger",
+        "google-cloud-logging",
+        "google-auth",
+    ]
+)
 
 requirements_path = "requirements.txt"
 requirements_file = open(requirements_path, "w")
