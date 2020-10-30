ada06d7159089b19d6606b14e27661414b11c26a
Niket <niket.shah1@ocado.com>
2020-10-30 11:15:20 +0000

    add support in django project for using the python logger
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- django_site/wsgi.py
+++ django_site/wsgi.py
@@ -8,7 +8,22 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import logging
+import google.cloud.logging
+from google.auth.exceptions import DefaultCredentialsError
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging_client = google.cloud.logging.Client()
+    logging_client.get_default_handler()
+    logging_client.setup_logging()
+except DefaultCredentialsError:
+    logging.info("No google credentials provided, not connecting google logging client")
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
 
 from lib.django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
