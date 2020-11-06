018190b286eb4baac2ed96ce22925e4d18c4561b
Razvan Mahu <razvan.mahu@ocado.com>
2020-11-06 18:00:10 +0000

    changed deploy.sh and try to use it

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,11 @@
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
-GCLOUD=${SEMAPHORE_CACHE_DIR}/google-cloud-sdk/bin/gcloud
-
-if [ ! -x ${GCLOUD} ]; then
-    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-159.0.0-linux-x86_64.tar.gz
-    tar zxf google-cloud-sdk-159.0.0-linux-x86_64.tar.gz -C ${SEMAPHORE_CACHE_DIR}
-    rm google-cloud-sdk-159.0.0-linux-x86_64.tar.gz
-fi
-
-${GCLOUD} --quiet components update
-${GCLOUD} auth activate-service-account --key-file .gcloud-key
-
 export MODULE_NAME=$1
 export VERSION="$2"
 export DATABASE_POSTFIX="$3"
 export DATABASE_NAME="cfl_${DATABASE_POSTFIX}"
 export CACHE_PREFIX="${MODULE_NAME}-"
-export GOOGLE_APPLICATION_CREDENTIALS=/home/runner/codeforlife-deploy-appengine/.gcloud-key
 
 # Install the dependencies for the following deploy script.
 # Kubernetes is a TEMPORARY solution. See issue 68.
@@ -27,8 +15,7 @@ pip install kubernetes
 pip install pyyaml
 
 # Authenticate the cluster by updating kubeconfig.
-${GCLOUD} config set project ${APP_ID}
-${GCLOUD} container clusters get-credentials ${MODULE_NAME} --zone europe-west1-b
+gcloud container clusters get-credentials ${MODULE_NAME} --zone europe-west1-b
 
 # Deploy the correct kubernetes cluster.
 python clusters_setup/deploy.py "${MODULE_NAME}"
@@ -56,14 +43,14 @@ fi
 
 envsubst <app.yaml.tmpl >app.yaml
 
-${GCLOUD} app --quiet deploy app.yaml --project ${APP_ID} --version ${VERSION} --no-promote
-${GCLOUD} app --quiet deploy cron.yaml --project ${APP_ID} --version ${VERSION} --no-promote
+gcloud app --quiet deploy app.yaml --project ${APP_ID} --version ${VERSION} --no-promote
+gcloud app --quiet deploy cron.yaml --project ${APP_ID} --version ${VERSION} --no-promote
 
 # Test the site
 ./test.sh ${MODULE_NAME} ${VERSION}
 
 # Promote
-${GCLOUD} app services set-traffic --project ${APP_ID} --splits ${VERSION}=1 ${MODULE_NAME} --migrate
+gcloud app services set-traffic --project ${APP_ID} --splits ${VERSION}=1 ${MODULE_NAME} --migrate
 
 # Test the site - again!
 ./test.sh ${MODULE_NAME} default
