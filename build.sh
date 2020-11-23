a2d9d4d5e368bcf7983ad0d800295aca42ba7fff
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-11-23 18:32:44 +0000

    Update branches to be new ones

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ rbenv rehash
 pip install requests
 pip install -t lib requests-toolbelt
 
-pip install -t lib --upgrade git+https://github.com/ocadotechnology/codeforlife-portal@django_2_0_test
-pip install -t lib --upgrade git+https://github.com/ocadotechnology/rapid-router@django_2_0_test
+pip install -t lib --upgrade git+https://github.com/ocadotechnology/codeforlife-portal@django_2_upgrade
+pip install -t lib --upgrade git+https://github.com/ocadotechnology/rapid-router@django_2_upgrade
 
 pip install -t lib django-anymail[amazon_ses]
 pip install -t lib google-auth==1.*
@@ -16,7 +16,7 @@ if [ "$ENVIRONMENT" = "default" ]
 then
     pip install -t lib --upgrade --no-deps aimmo
 else
-    pip install -t lib --pre --upgrade --no-deps git+https://github.com/ocadotechnology/aimmo@django_2_0_test
+    pip install -t lib --pre --upgrade --no-deps git+https://github.com/ocadotechnology/aimmo@django_2_upgrade
 fi
 
 pip uninstall cfl-common
