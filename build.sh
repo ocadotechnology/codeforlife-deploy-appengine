5b65b07a90c1e0f4bc08d3783af671f33d20b65d
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-11-23 18:38:39 +0000

    Install autoconfig

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ rbenv rehash
 pip install requests
 pip install -t lib requests-toolbelt
 
+pip install django-autoconfig
+
 pip install -t lib --upgrade git+https://github.com/ocadotechnology/codeforlife-portal@django_2_upgrade
 pip install -t lib --upgrade git+https://github.com/ocadotechnology/rapid-router@django_2_upgrade
 
@@ -19,8 +21,7 @@ else
     pip install -t lib --pre --upgrade --no-deps git+https://github.com/ocadotechnology/aimmo@django_2_upgrade
 fi
 
-pip uninstall cfl-common
-pip install -t lib --upgrade "git+https://github.com/ocadotechnology/codeforlife-portal@django_2_0_test#egg=cfl-common&subdirectory=cfl_common"
+pip install -t lib --upgrade "git+https://github.com/ocadotechnology/codeforlife-portal@django_2_upgrade#egg=cfl-common&subdirectory=cfl_common"
 
 python generate_requirements.py
 
