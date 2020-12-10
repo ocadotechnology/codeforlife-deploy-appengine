f6138c6b98929b96230f8c63d8293fd8b765d004
Niket <niket.shah1@ocado.com>
2020-12-10 15:56:41 +0000

    try another attempt at gke connection
    Signed-off-by: Niket <niket.shah1@ocado.com>

---
+++ django_site/apps.py
@@ -0,0 +1,36 @@
+from django.apps import AppConfig
+from google.auth import compute_engine
+from google.cloud.container_v1 import ClusterManagerClient
+from kubernetes import client
+
+
+class AppEngineAppConfig(AppConfig):
+    name = "django_site"
+    verbose_name = "Code for Life"
+
+    def setup_gke(self):
+        project_id = "decent-digit-629"
+        zone = "europe-west1-b"
+        cluster_id = "agones-terraform-dev"
+
+        credentials = compute_engine.Credentials()
+
+        cluster_manager_client = ClusterManagerClient(credentials=credentials)
+        cluster = cluster_manager_client.get_cluster(project_id, zone, cluster_id)
+
+        configuration = client.Configuration()
+        configuration.host = f"https://{cluster.endpoint}:443"
+        configuration.verify_ssl = False
+        configuration.api_key = {"authorization": "Bearer " + credentials.token}
+        client.Configuration.set_default(configuration)
+
+    def ready(self):
+        self.setup_gke()
+
+        v1 = client.CoreV1Api()
+        print("Listing pods with their IPs:")
+        pods = v1.list_pod_for_all_namespaces(watch=False)
+        for i in pods.items:
+            print(
+                "%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name)
+            )
