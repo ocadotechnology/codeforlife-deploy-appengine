c1f946a83faa2a2cc42212cc19962256793b0975
Niket <niket.shah1@ocado.com>
2020-12-11 11:41:18 +0000

    load kubeconfgi from dict
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- django_site/apps.py
+++ django_site/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 from google.auth import compute_engine
 from google.cloud.container_v1 import ClusterManagerClient
 from kubernetes import client, config
-
+import yaml
 
 # class AppEngineAppConfig(AppConfig):
 #     name = "django_site"
@@ -16,10 +16,12 @@ def setup_gke():
     # cluster_id = "agones-terraform-dev"
 
     # credentials = compute_engine.Credentials()
-    contexts, active_context = config.list_kube_config_contexts()
-    print(contexts)
-    print(active_context)
+    # contexts, active_context = config.list_kube_config_contexts()
+    # print(contexts)
+    # print(active_context)
     # config.load_kube_config()
+    config_dict = yaml.load("../kubeconfig.yaml")
+    config.load_kube_config_from_dict(config_dict)
     # cluster_manager_client = ClusterManagerClient(credentials=credentials)
     # cluster = cluster_manager_client.get_cluster(
     #     name=f"projects/{project_id}/locations/{zone}/clusters/{cluster_id}"
@@ -33,8 +35,8 @@ def setup_gke():
 
     # def ready(self):
 
-    # v1 = client.CoreV1Api()
-    # print("Listing pods with their IPs:")
-    # pods = v1.list_pod_for_all_namespaces(watch=False)
-    # for i in pods.items:
-    #     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    pods = v1.list_pod_for_all_namespaces(watch=False)
+    for i in pods.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
