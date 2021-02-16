d2f96a1783c8d903cdf2f89102b0fe08422dc72c
Razvan Mahu <razvan.mahu@ocado.com>
2021-02-16 14:22:24 +0000

    enable clusters setup

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,7 @@ ${GCLOUD} config set project ${APP_ID}
 ${GCLOUD} container clusters get-credentials aimmo-${MODULE_NAME} --zone europe-west1-b
 
 # Deploy the correct kubernetes cluster.
-# python clusters_setup/deploy.py "${MODULE_NAME}"
+python clusters_setup/deploy.py "${MODULE_NAME}"
 
 ./manage.py migrate --no-input
 
