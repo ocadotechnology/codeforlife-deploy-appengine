b1da76ef12c8e6d24d3be5c7823ec5341ebe2645
Razvan Mahu <67904187+razvan-pro@users.noreply.github.com>
2021-03-01 16:58:40 +0000

    Update agones version (#234)
    * fixed agones version and enabled monitoring
    
    * updated staging
    
    * added comments with source link

--- clusters_setup/terraform/main.tf
+++ clusters_setup/terraform/main.tf
@@ -54,20 +54,101 @@ module "gke_cluster" {
   }
 }
 
-module "helm_agones" {
-  // ***************************************************************************************************
-  // Update ?ref= to the agones release you are installing. For example, ?ref=release-1.8.0 corresponds
-  // to Agones version 1.8.0
-  // ***************************************************************************************************
-  source = "git::https://github.com/googleforgames/agones.git//install/terraform/modules/helm3/?ref=release-1.12.0"
+resource "helm_release" "agones" {
+  // Inspired from https://github.com/googleforgames/agones/blob/main/install/terraform/modules/helm3/helm.tf
+  name             = "agones"
+  repository       = "https://agones.dev/chart/stable"
+  force_update     = var.force_update
+  chart            = var.chart
+  timeout          = 420
+  version          = local.agones_version
+  namespace        = "agones-system"
+  create_namespace = true
 
-  agones_version         = local.agones_version
-  values_file            = ""
-  feature_gates          = var.feature_gates
-  host                   = module.gke_cluster.host
-  token                  = module.gke_cluster.token
-  cluster_ca_certificate = module.gke_cluster.cluster_ca_certificate
-  log_level              = var.log_level
+  # Use terraform of the latest >=0.12 version
+  values = [
+    length(var.values_file) == 0 ? "" : file(var.values_file),
+  ]
+
+  set {
+    name  = "agones.metrics.stackdriverEnabled"
+    value = "true"
+  }
+
+  set {
+    name  = "agones.metrics.prometheusEnabled"
+    value = "false"
+  }
+
+  set {
+    name  = "agones.metrics.prometheusServiceDiscovery"
+    value = "false"
+  }
+
+  set {
+    name  = "crds.CleanupOnDelete"
+    value = var.crd_cleanup
+  }
+
+  set {
+    name  = "agones.image.registry"
+    value = var.image_registry
+  }
+
+  set {
+    name  = "agones.image.controller.pullPolicy"
+    value = var.pull_policy
+  }
+
+  set {
+    name  = "agones.image.sdk.alwaysPull"
+    value = var.always_pull_sidecar
+  }
+
+  set {
+    name  = "agones.image.controller.pullSecret"
+    value = var.image_pull_secret
+  }
+
+  set {
+    name  = "agones.ping.http.serviceType"
+    value = var.ping_service_type
+  }
+
+  set {
+    name  = "agones.ping.udp.expose"
+    value = var.udp_expose
+  }
+
+  set {
+    name  = "agones.ping.udp.serviceType"
+    value = var.ping_service_type
+  }
+
+  set {
+    name  = "agones.controller.logLevel"
+    value = var.log_level
+  }
+
+  set {
+    name  = "agones.featureGates"
+    value = var.feature_gates
+  }
+
+  set {
+    name  = "gameservers.namespaces"
+    value = "{${join(",", var.gameserver_namespaces)}}"
+  }
+
+  set {
+    name  = "gameservers.minPort"
+    value = var.gameserver_minPort
+  }
+
+  set {
+    name  = "gameservers.maxPort"
+    value = var.gameserver_maxPort
+  }
 }
 
 data "google_compute_address" "load_balancer_ip" {
