fda021637172fea695177676d3d48941f9ec0347
Razvan Mahu <razvan.mahu@ocado.com>
2021-06-29 17:49:33 +0100

    WIP: deploy portal branch

--- generate_requirements.py
+++ generate_requirements.py
@@ -1,11 +1,20 @@
+from pathlib import Path
+
 # Versions will be fetched from the init of each package (portal, aimmo, game) after they are installed in lib - this happens in the "Build" step.
 from lib.portal import __version__ as portal_version
 from lib.aimmo import __version__ as aimmo_version
 from lib.game import __version__ as rapid_router_version
 
+portal_requirement = f"codeforlife-portal=={portal_version}"
+common_requirement = ""
+if Path("build/codeforlife-portal").is_dir():
+    portal_requirement = "./build/codeforlife-portal"
+    common_requirement = "./build/codeforlife-portal/cfl_common"
+
 requirements = "\n".join(
     [
-        f"codeforlife-portal=={portal_version}",
+        portal_requirement,
+        common_requirement,
         f"aimmo=={aimmo_version}",
         # "./aimmo",  # Uncomment this to install a custom aimmo built in deploy_gcloud workflow
         f"rapid-router=={rapid_router_version}",
