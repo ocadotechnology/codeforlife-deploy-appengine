5b0f6aa46b18138e8f52929c7bf5a7dc2e9a5f94
Razvan Mahu <razvan.mahu@ocado.com>
2021-07-21 14:16:19 +0100

    load kube config in manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+import kubernetes
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
 if __name__ == "__main__":
@@ -10,3 +12,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+    kubernetes.config.load_kube_config("django_site/kubeconfig.yaml")
