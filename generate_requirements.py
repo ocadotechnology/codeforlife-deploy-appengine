75a5df305f2df4fb4adaa5234e22f3c161c3ae24
Razvan Mahu <67904187+razvan-pro@users.noreply.github.com>
2021-09-08 17:43:49 +0100

    feat: Deploy any branch (#250)
    * WIP: deploy portal branch
    
    * deploy portal from branch
    
    * fix bash assignment
    
    * fix generate_requirements
    
    * install rapid-router
    
    * install portal from branch in build step
    
    * egg is package name
    
    * more double quotes
    
    * try without double quotes
    
    * Merge branch 'master' into deploy-any-branch
    
    * add custom rapid-router branch
    
    * try rapid-router branch as input
    
    * add portal branch as input
    
    * add aimmo branch input (only for the main package)
    
    * no : after else in bash
    
    * pass aimmo-branch to the build step
    
    * load nvm
    
    * fix: RR reorder
    
    * also install rr from branch in lib
    
    * clear the cache
    
    * install django-redis
    
    * alternative to clear the cache
    
    * Revert "alternative to clear the cache"
    
    This reverts commit 58b235df908749e1764142a5a1c183f3157ae034.
    
    * Revert "install django-redis"
    
    This reverts commit 9f4ab456c963924ff25c4d82f955a41e219b6389.
    
    * Revert "clear the cache"
    
    This reverts commit 2dfb6a134b857bade8c2454e88981d7ff0faf5b0.
    
    * cleaned up generate_requirements
    
    * Merge branch 'master' into deploy-any-branch
    
    * code review
    
    * top import portal in generate_requirements

--- generate_requirements.py
+++ generate_requirements.py
@@ -1,14 +1,40 @@
+import argparse
+
 # Versions will be fetched from the init of each package (portal, aimmo, game) after they are installed in lib - this happens in the "Build" step.
-from lib.portal import __version__ as portal_version
 from lib.aimmo import __version__ as aimmo_version
 from lib.game import __version__ as rapid_router_version
+from lib.portal import __version__ as portal_version
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--portal-branch")
+parser.add_argument("--rapid-router-branch")
+parser.add_argument("--aimmo-branch")
+args = parser.parse_args()
+
+if args.portal_branch:
+    portal_requirement = f"git+https://github.com/ocadotechnology/codeforlife-portal@{args.portal_branch}#egg=codeforlife-portal"
+    common_requirement = f"git+https://github.com/ocadotechnology/codeforlife-portal@{args.portal_branch}#egg=cfl-common&subdirectory=cfl_common"
+else:
+    portal_requirement = f"codeforlife-portal=={portal_version}"
+    common_requirement = ""
+
+if args.rapid_router_branch:
+    rapid_router_requirement = f"git+https://github.com/ocadotechnology/rapid-router@{args.rapid_router_branch}#egg=rapid-router"
+else:
+    rapid_router_requirement = f"rapid-router=={rapid_router_version}"
+
+if args.aimmo_branch:
+    aimmo_requirement = "./aimmo"
+else:
+    aimmo_requirement = f"aimmo=={aimmo_version}"
 
 requirements = "\n".join(
     [
-        f"codeforlife-portal=={portal_version}",
-        f"aimmo=={aimmo_version}",
-        # "./aimmo",  # Uncomment this to install a custom aimmo built in deploy_gcloud workflow
-        f"rapid-router=={rapid_router_version}",
+        rapid_router_requirement,
+        portal_requirement,
+        common_requirement,
+        aimmo_requirement,
         "requests-toolbelt==0.9.*",
         "mysqlclient==1.4.*",
         "redis==3.3.*",
