aa070983fb561301b60867b54b0240d11163bac1
Razvan Mahu <razvan.mahu@ocado.com>
2022-01-27 10:58:59 +0000

    load kube config periodically

--- django_site/apps.py
+++ django_site/apps.py
@@ -1,12 +1,34 @@
 import os
+import logging
+import time
+from threading import Thread
 
 import yaml
 from kubernetes import config
 
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 def setup_gke():
     filename = os.path.join(CURR_DIR, "kubeconfig.yaml")
     config_dict = yaml.safe_load(open(filename, "r").read())
-    config.load_kube_config_from_dict(config_dict)
+    config_reload_interval = 15 * 60  # 15 minutes
+
+    def load_kube_config_indefinitely(interval):
+        while True:
+            try:
+                logging.info("Loading kube config")
+                config.load_kube_config_from_dict(config_dict)
+            except:
+                logging.exception("Exception when calling load_kube_config_from_dict")
+            time.sleep(interval)
+
+    # Start a thread that loads kube config periodically
+    thread = Thread(
+        target=load_kube_config_indefinitely,
+        args=(config_reload_interval,),
+        daemon=True,
+    )
+    thread.start()
