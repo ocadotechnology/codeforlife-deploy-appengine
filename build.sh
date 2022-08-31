857f9a0ccd85631325fc7ce2c5475785404e42d5
faucomte97 <florian.aucomt1@ocado.com>
2022-08-31 16:18:49 +0100

    Revert

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rbenv rehash
 pip install requests
 pip install -t lib requests-toolbelt
 
-pip install -t lib git+https://github.com/ocadotechnology/django-autoconfig
+#pip install -t lib git+https://github.com/ocadotechnology/django-autoconfig
 pip install -t lib --upgrade codeforlife-portal
 
 pip install -t lib django-anymail[amazon_ses]
