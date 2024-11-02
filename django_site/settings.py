e63107c7b80b199ad8a8383c252e5068ce529acb
faucomte97 <f.aucomte@hotmail.co.uk>
2024-11-02 01:08:49 +0000

    Make PSQL settings default

--- django_site/settings.py
+++ django_site/settings.py
@@ -169,10 +169,10 @@ ALLOWED_HOSTS = [".appspot.com", ".codeforlife.education"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.getenv("PSQL_DATABASE_HOST"),
-        "NAME": os.getenv("PSQL_DATABASE_NAME"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "NAME": os.getenv("DATABASE_NAME"),
         "USER": "postgres",
-        "PASSWORD": os.getenv("PSQL_DATABASE_PASSWORD"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "ATOMIC_REQUESTS": True,
     }
 }
