6973ee96063714506fe977f5f1930970530e9913
SKairinos <skairinos@gmail.com>
2026-02-19 16:25:55 +0000

    fix

--- generate_requirements.py
+++ generate_requirements.py
@@ -1,8 +1,8 @@
 import argparse
 
 # Versions will be fetched from the init of each package (portal, game) after they are installed in lib - this happens in the "Build" step.
-from lib.game import __version__ as rapid_router_version
-from lib.portal import __version__ as portal_version
+# from lib.game import __version__ as rapid_router_version
+# from lib.portal import __version__ as portal_version
 
 
 parser = argparse.ArgumentParser()
@@ -12,7 +12,7 @@ args = parser.parse_args()
 
 if args.portal_branch:
     portal_requirement = f"git+https://github.com/ocadotechnology/codeforlife-portal@{args.portal_branch}#egg=codeforlife-portal"
-    common_requirement = f"git+https://github.com/ocadotechnology/codeforlife-portal@{args.portal_branch}#egg=cfl-common&subdirectory=cfl_common"
+    common_requirement = "git+https://github.com/ocadotechnology/codeforlife-portal@553abd7556b6e8bf00622515b3d127e9fe68b859#egg=cfl-common&subdirectory=cfl_common"
 else:
     portal_requirement = f"codeforlife-portal=={portal_version}"
     common_requirement = f"cfl-common=={portal_version}"
@@ -28,10 +28,10 @@ requirements = "\n".join(
         portal_requirement,
         common_requirement,
         "requests-toolbelt==1.0.0",
-        "redis==5.2.0",
+        "redis==5.2.1",
         "django-redis==5.4.0",
         "google-cloud-logging==1.*",
-        "google-auth==1.*",
+        "google-auth==2.*",
         "psycopg2==2.9.10",
     ]
 )
